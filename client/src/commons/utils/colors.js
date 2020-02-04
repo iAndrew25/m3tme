74a26a4d02f9a04c66bedc852c8cfd72c168add5
Andrei Otea <otea@arezzosky.com>
2020-02-04 17:11:51 +0200

    Colors

--- client/src/commons/utils/colors.js
+++ client/src/commons/utils/colors.js
@@ -7,6 +7,17 @@ const themes = {
 		background: '#ECEDEA',
 		white: '#FFFFFF',
 	},
+	orange: {
+		darkPrimary: '#E64A19',
+		defaultPrimary: '#FF5722',
+		lightPrimary:'#FFCCBC',
+		textPrimary: '#FFFFFF',
+		accent: '#536DFE',
+		primaryText: '#212121',
+		secondaryText: '#757575',
+		divider: '#BDBDBD',
+		background: '#FAFAFA'
+	}
 };
 
 export default color => themes.white[color];
